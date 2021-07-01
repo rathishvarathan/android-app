ac5f74f8ef229710ab7c289613e7afa6e3a07703
rathishvarathan <86611355+rathishvarathan@users.noreply.github.com>
2021-07-01 03:23:01 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,46 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restart
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage  extends StatelessWidget{
+  bool selected=false;
+  @override
+  Widget build(BuildContext  context) {
+    return Scaffold(
+      body: Center(child: Column(
+        children: [
+          ElevatedButton(onPressed: (){
+            selected=true;
+          }, child: Text("click here for change")),
+           Text("data",style: TextStyle(color:Colors.green,fontSize: 35,)),
+        ],
+      )));
+}
+}
